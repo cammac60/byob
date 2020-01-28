33e10cfebf4103e302236324b29d903ab9adae29
Cameron MacRae <cam.mac.cm@gmail.com>
2020-01-27 18:03:19 -0700

    Initial commit

---
+++ teams.js
@@ -0,0 +1,212 @@
+const teams = [
+    {
+      "id": 1,
+      "name": "New Jersey Devils",
+      "venue": "Prudential Center",
+      "abbreviation": "NJD",
+      "roster": []
+    },
+    {
+      "id": 2,
+      "name": "New York Islanders",
+      "venue": "Barclays Center",
+      "abbreviation": "NYI",
+      "roster": []
+    },
+    {
+      "id": 3,
+      "name": "New York Rangers",
+      "venue": "Madison Square Garden",
+      "abbreviation": "NYR",
+      "roster": []
+    },
+    {
+      "id": 4,
+      "name": "Philadelphia Flyers",
+      "venue": "Wells Fargo Center",
+      "abbreviation": "PHI",
+      "roster": []
+    },
+    {
+      "id": 5,
+      "name": "Pittsburgh Penguins",
+      "venue": "PPG Paints Arena",
+      "abbreviation": "PIT",
+      "roster": []
+    },
+    {
+      "id": 6,
+      "name": "Boston Bruins",
+      "venue": "TD Garden",
+      "abbreviation": "BOS",
+      "roster": []
+    },
+    {
+      "id": 7,
+      "name": "Buffalo Sabres",
+      "venue": "KeyBank Center",
+      "abbreviation": "BUF",
+      "roster": []
+    },
+    {
+      "id": 8,
+      "name": "Montréal Canadiens",
+      "venue": "Bell Centre",
+      "abbreviation": "MTL",
+      "roster": []
+    },
+    {
+      "id": 10,
+      "name": "Toronto Maple Leafs",
+      "venue": "Scotiabank Arena",
+      "abbreviation": "TOR",
+      "roster": []
+    },
+    {
+      "id": 12,
+      "name": "Carolina Hurricanes",
+      "venue": "PNC Arena",
+      "abbreviation": "CAR",
+      "roster": []
+    },
+    {
+      "id": 13,
+      "name": "Florida Panthers",
+      "venue": "BB&T Center",
+      "abbreviation": "FLA",
+      "roster": []
+    },
+    {
+      "id": 14,
+      "name": "Tampa Bay Lightning",
+      "venue": "AMALIE Arena",
+      "abbreviation": "TBL",
+      "roster": []
+    },
+    {
+      "id": 15,
+      "name": "Washington Capitals",
+      "venue": "Capital One Arena",
+      "abbreviation": "WSH",
+      "roster": []
+    },
+    {
+      "id": 16,
+      "name": "Chicago Blackhawks",
+      "venue": "United Center",
+      "abbreviation": "CHI",
+      "roster": []
+    },
+    {
+      "id": 17,
+      "name": "Detroit Red Wings",
+      "venue": "Little Caesars Arena",
+      "abbreviation": "DET",
+      "roster": []
+    },
+    {
+      "id": 18,
+      "name": "Nashville Predators",
+      "venue": "Bridgestone Arena",
+      "abbreviation": "NSH",
+      "roster": []
+    },
+    {
+      "id": 19,
+      "name": "St. Louis Blues",
+      "venue": "Enterprise Center",
+      "abbreviation": "STL",
+      "roster": []
+    },
+    {
+      "id": 20,
+      "name": "Calgary Flames",
+      "venue": "Scotiabank Saddledome",
+      "abbreviation": "CGY",
+      "roster": []
+    },
+    {
+      "id": 21,
+      "name": "Colorado Avalanche",
+      "venue": "Pepsi Center",
+      "abbreviation": "COL",
+      "roster": []
+    },
+    {
+      "id": 22,
+      "name": "Edmonton Oilers",
+      "venue": "Rogers Place",
+      "abbreviation": "EDM",
+      "roster": []
+    },
+    {
+      "id": 23,
+      "name": "Vancouver Canucks",
+      "venue": "Rogers Arena",
+      "abbreviation": "VAN",
+      "roster": []
+    },
+    {
+      "id": 24,
+      "name": "Anaheim Ducks",
+      "venue": "Honda Center",
+      "abbreviation": "ANA",
+      "roster": []
+    },
+    {
+      "id": 25,
+      "name": "Dallas Stars",
+      "venue": "American Airlines Center",
+      "abbreviation": "DAL",
+      "roster": []
+    },
+    {
+      "id": 26,
+      "name": "Los Angeles Kings",
+      "venue": "STAPLES Center",
+      "abbreviation": "LAK",
+      "roster": []
+    },
+    {
+      "id": 28,
+      "name": "San Jose Sharks",
+      "venue": "SAP Center at San Jose",
+      "abbreviation": "SJS",
+      "roster": []
+    },
+    {
+      "id": 29,
+      "name": "Columbus Blue Jackets",
+      "venue": "Nationwide Arena",
+      "abbreviation": "CBJ",
+      "roster": []
+    },
+    {
+      "id": 30,
+      "name": "Minnesota Wild",
+      "venue": "Xcel Energy Center",
+      "abbreviation": "MIN",
+      "roster": []
+    },
+    {
+      "id": 52,
+      "name": "Winnipeg Jets",
+      "venue": "Bell MTS Place",
+      "abbreviation": "WPG",
+      "roster": []
+    },
+    {
+      "id": 53,
+      "name": "Arizona Coyotes",
+      "venue": "Gila River Arena",
+      "abbreviation": "ARI",
+      "roster": []
+    },
+    {
+      "id": 54,
+      "name": "Vegas Golden Knights",
+      "venue": "T-Mobile Arena",
+      "abbreviation": "VGK",
+      "roster": []
+    }
+  ]
